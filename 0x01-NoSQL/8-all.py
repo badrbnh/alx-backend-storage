45351d88fe8c9e4d03f150c3ef1adabcaf919e69
badreddine <badrechahide@gmail.com>
2024-01-22 08:27:17 -0800

    task

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""function that lists all documents in a collection:"""
+
+
+def list_all(mongo_collection):
+    """function that lists all documents in a collection:"""
+    return mongo_collection.find()
+
