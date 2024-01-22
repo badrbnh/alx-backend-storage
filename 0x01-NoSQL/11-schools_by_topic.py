e8cf0be37fc5568cfc2e3a2c80abbf4edc2a4822
badreddine <badrechahide@gmail.com>
2024-01-22 08:57:05 -0800

    task

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,4 +4,9 @@
 
 def schools_by_topic(mongo_collection, topic):
     """function that returns the list of school having a specific topic:"""
-    return mongo_collection.find({topics: topic})
+    cursor = mongo_collection.find({'topics': topic}, {'_id': 0, 'name': 1})
+    
+    # Extract the names of schools from the cursor
+    school_names = [document['name'] for document in cursor]
+    
+    return school_names
