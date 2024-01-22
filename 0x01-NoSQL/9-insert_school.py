233fd75ee7b0c24496fa155409058f91d0feb26a
badreddine <badrechahide@gmail.com>
2024-01-22 08:35:20 -0800

    task

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""Python function that inserts a new document in a collection based on kwargs"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """function that inserts a new document in a collection based on kwargs"""
+    query = mongo_collection.insert(kwargs)
+    return query.get("_id")
