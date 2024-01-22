1a30c9f6464288bbb6bde6b6414f76e5a6fd36c6
badreddine <badrechahide@gmail.com>
2024-01-22 08:42:11 -0800

    task

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""function that changes all topics of a school document based on the name"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """function that changes all topics of a school document based on the name"""
+    return mongo_collection.update(name, topics)
+
